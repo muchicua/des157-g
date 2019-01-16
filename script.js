520334160b7145adc6d252c70514b43a4e38cc3c
Margot Uchicua <46686016+muchicua@users.noreply.github.com>
2019-01-16 02:21:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
 console.log('reading js');
-// var l,a;
-// var new color [] colors= { rgb(249, 190, 131) , rgb(247, 155, 119), rgb(244, 124, 108), rgb(232, 97, 104), rgb(212, 73, 98), rgb(193, 58, 99)};
-function setup(){
+var l, a, i;
+var colors = ["#C460E0",
+         "#0256A7",
+         "#69F5E7",
+         "#687DF2",
+         "#69F591",
+         "#F1Ea67"];
+// var colors = ['249, 190, 131', '247, 155, 119', '244, 124, 108', '232, 97, 104', '212, 73, 98', '193, 58, 99)'];
+
+function setup() {
+  // variables initialization
+  l = 0;
+  a = 250;
+
+  // background(#ffffff);
+    strokeWeight(2);
+
   //size is createCanvas in P5
   // create a variable to reference the canvas
-  // l=0;
-  // a=250;
-
-  var myCanvas = createCanvas(800,250);
-  // background(#ffffff);
-
+  var myCanvas = createCanvas(800, 250);
   // connect my Canvas to mySketch
   myCanvas.parent("MySketch");
 }
-function draw(){
-  // strokeWeight(2);
-  // stroke(color[mousex/160]);
-  // line(mouseX,a,pmouseX,l)
-   fill(113, 204, 98);
-   ellipse(mouseX,mouseY,50,50);
+
+function draw() {
+  //transparent rectangle for fading
+  fill(255,255,255,3); // this is correct
+  rect(-2,-2,802,255);
+
+
+
+  // update the index value
+  i = Math.floor(mouseX/160); // Math.floor truncates the decimal
+  console.log('i is: ' + i);
+  stroke(colors[i]);
+  line(mouseX,a,pmouseX,l)
+
 }
+
+
+// /* 01/12/19 */
+// // Move the mouse quickly to see the difference
+// // between the current and previous position
+// // color change
+// color[] colors = {#F8BD82, #F79A76, #F47C6C, #E86269, #D44962, #C03A63};
+//
+// int l,a;
+//
+//
+// void setup(){
+// l=0;
+// a=250;
+// size(800,250);
+// background(#ffffff);
+// frameRate(60);
+// }
+//
+// void draw() {
+// fill(255,255,255,2);
+// rect(-1,-1,801,256);
+// strokeWeight(3);
+// stroke(colors[mouseX/160]);
+// line(mouseX, a, pmouseX, l);
+//
+// }
